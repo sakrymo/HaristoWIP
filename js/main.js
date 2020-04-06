53eda0228a6d098c93908c89217045fc9d84dad8
Sakrymo <kr.moskal@email.com>
2020-04-06 21:04:28 +0200

    -

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
-const preloader = document.querySelector(".preloader_wrapper");
-const videoBackground = document.querySelector("#hero_animation");
-const heroSection = document.querySelector('.hero_section');
+const preloader = document.querySelector(".preloader-container");
+const videoBackground = document.querySelector("#hero-animation");
+const heroSection = document.querySelector('.hero-section');
 const sectionA = document.querySelector('#section-A')
 const navbar = document.querySelector('#navbar');
 const mapAnimation = document.querySelector('#map-animation')
 const sectionABreakpoint = document.querySelector('#section-A-breakpoint')
-const scrollIndicator = document.querySelector('.scrollindicator_container')
+const scrollIndicator = document.querySelector('.scrollindicator-container')
+const checkboxLeft = document.querySelector('#menu-left');
+const checkboxRight = document.querySelector('#menu-right');
+const fsMenu = document.querySelector('#fs-menu');
 
-window.onscroll = function() {showSections()};
+window.onscroll = function () {
+  showSections()
+};
 
 // Page loading
 // document.onload(pageLoaded());
@@ -19,8 +24,8 @@ function pageLoaded() {
   scrollIndicator.classList.add("fade-in")
 };
 
-scrollIndicator.addEventListener('click', function(){
-  element.scrollIntoView(sectionABreakpoint)
+scrollIndicator.addEventListener('click', function () {
+  sectionABreakpoint.scrollIntoView();
 });
 
 // Section transitions
@@ -31,12 +36,44 @@ function showSections() {
     heroSection.classList.add("hidden");
     navbar.classList.add("show-navbar");
     mapAnimation.play();
-  }
-
-  else {
+  } else {
     sectionA.classList.remove("visible-section");
     navbar.classList.remove("show-navbar")
     heroSection.classList.remove("hidden");
     mapAnimation.pause();
   }
 }
+
+function toggleMenu(checkbox) {
+
+  if (checkbox === checkboxLeft) {
+    if (checkbox.checked === true && sectionA.classList.contains('visible-section') !== true) {
+      console.log(`Left menu is now active: ${checkbox.checked}`);
+      document.querySelector('body').style = "overflow: hidden;";
+      fsMenu.classList.add('active-menu');
+      checkboxLeft.parentElement.classList.add('ontop');
+    } else if (checkbox.checked === false) {
+      console.log(`Left menu is now active: ${checkbox.checked}`);
+      document.querySelector('body').style = "overflow: visible;";
+      fsMenu.classList.remove('active-menu');
+    }
+  }
+
+  if (checkbox === checkboxRight) {
+    if (checkbox.checked === true && sectionA.classList.contains('visible-section') === true) {
+      console.log(`Right menu is now active: ${checkbox.checked}`);
+      document.querySelector('body').style = "overflow: hidden;";
+    } else if (checkbox.checked === false) {
+      console.log(`Right menu is now active: ${checkbox.checked}`);
+      document.querySelector('body').style = "overflow: visible;";
+    }
+  }
+}
+
+// Menu triggers
+checkboxLeft.addEventListener('click', function () {
+  toggleMenu(checkboxLeft)
+});
+checkboxRight.addEventListener('click', function () {
+  toggleMenu(checkboxRight)
+});

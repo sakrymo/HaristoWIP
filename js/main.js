2a41411953fbf08a3475132ae827fcc9e299ee84
Sakrymo <kr.moskal@email.com>
2020-04-16 18:19:43 +0200

    s

--- js/main.js
+++ js/main.js
@@ -93,7 +93,7 @@ checkboxRight.addEventListener('click', function () {
   toggleMenu(checkboxRight)
 });
 
-if (mobileView.style.opacity === 1) {
+if (screen && screen.width < 768) {
   particlesJS.load('particles-js', 'js/particlesjs-config.json', function() {
     console.log('callback - particles.js config loaded');
   });

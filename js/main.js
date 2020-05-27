a3f95cdccea0d4cd45f4174984f81cc2a686eeda
Sakrymo <kr.moskal@email.com>
2020-05-27 20:41:21 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ window.onscroll = function () {
   showSections()
 };
 
-videoBackground.addEventListener('loadeddata', function() {
+videoBackground.addEventListener('canplay', function() {
   videoBackground.play();
   preloader.classList.add("page-loaded");
   mapAnimation.pause();

ff7a36b371002057216d557bc4dda561923eb351
Sakrymo <kr.moskal@email.com>
2020-05-30 00:02:59 +0200

    Update todo.rb

--- todo.rb
+++ todo.rb
@@ -2,13 +2,13 @@
 
 HIGH PRIORITY
 [] move content to mobile website
-[] make davinci section
+[x] make davinci section
 [] make the animation for partners members etc
 [] add a working contact form and redirect to main site as last section
 [] add a list of countries in first section
+[x] working buttons
 
 MEDIUM PRIORITY
-[] Make preloader stop only when the video is loaded progress bar would be cool
 [] Optimize video and images to load as quickly as possible
 [] Add arrows to video players somehow
 
@@ -22,10 +22,13 @@ LOW PRIORITY
 
 Current changes
 - Added new video of Da Vinci
-- Preloader closes only when video can play
+- Buttons work
 
 
 =end
 
 # if section B is visible and DaVinci is less than halfway through then play and later pause the video
 # if section B is not visible and DaVinci is at half then play from half to end
+
+
+# TRY https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setInterval
